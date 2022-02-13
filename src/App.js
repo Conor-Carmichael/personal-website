d7e536c1d6657cc2ec7e7a4129d14b95cd7dd947
Conor-Carmichael <conorbcarmichael@gmail.com>
2022-02-13 12:16:31 -0500

    Bootstrap import, interest comp init

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import SiteNav from './Components/SiteNav';
 import Projects from './Components/Projects'
 import Education from './Components/Education';
 import MScProject from './Components/MScProject';
+import Experience from './Components/Experience';
+import Interests from './Components/Interests';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Route path='/projects' element={<Projects />}      />
         <Route path='/education' element={<Education />}    />
         <Route path='/msc-project' element={<MScProject />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='/interests' element={<Interests />} />
           
       </Routes>
       
