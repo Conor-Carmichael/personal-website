60d58597ea513a3cdaa1131bfc831e815a140375
Conor-Carmichael <conorbcarmichael@gmail.com>
2022-02-13 10:33:22 -0500

    Basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import SiteNav from './Components/SiteNav';
+import Projects from './Components/Projects'
+import Education from './Components/Education';
+import MScProject from './Components/MScProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <SiteNav />
+
+      <Routes>
+        <Route path='/projects' element={<Projects />}      />
+        <Route path='/education' element={<Education />}    />
+        <Route path='/msc-project' element={<MScProject />} />
+          
+      </Routes>
+      
+    </Router>
+
+
   );
 }
 
