f00466c503e7452f997d3c891b8953a8b72b90ae
Conor-Carmichael <conorbcarmichael@gmail.com>
2022-02-13 13:53:38 -0500

    Removed unneeded extension

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -5,36 +5,33 @@ import { Component } from "react/cjs/react.production.min";
 import postgradCourses from '../Data/postgraduate_courses.json';
 import undergradCourses from '../Data/undergraduate_courses.json';
 
-class Education extends Component {
+const Education = () => {
+    
+    let undergradList = undergradCourses.map(function(obj, i){
+        return <li><i>{obj.season.charAt(0).toUpperCase()} {obj.year}</i> {obj.department} {obj.course_number}: {obj.course_title}</li>
+    })
 
-    render() {
-        
-        let undergradList = undergradCourses.map(function(obj, i){
-            return <li><i>{obj.season.charAt(0).toUpperCase()} {obj.year}</i> {obj.department} {obj.course_number}: {obj.course_title}</li>
-        })
-
-        let postgradList = postgradCourses.map(function(obj, i ) {
-            return <li><i>{obj.season.charAt(0).toUpperCase()} {obj.year}</i> {obj.department} {obj.course_title}</li>
-        })
-
-        return (
-            <Container>
-                <Row>
-                    <Col >
-                        <h2>University of Massachusetts Amherst</h2>
-                        <h4><i>Bachelors of Science, Computer Science</i></h4>
-                        <ul>{undergradList}</ul>
-                    </Col>
-                    <Col>
-                        <h2>University of Glasgow</h2>
-                        <h4><i>Masters of Science, Data Science</i></h4>
-                        <ul>{postgradList}</ul>
-                    </Col>
-                </Row>
-                
-            </Container>
-        )
-    }
+    let postgradList = postgradCourses.map(function(obj, i ) {
+        return <li><i>{obj.season.charAt(0).toUpperCase()} {obj.year}</i> {obj.department} {obj.course_title}</li>
+    })
+    return (        
+        <Container>
+            <Row>
+                <Col >
+                    <h2>University of Massachusetts Amherst</h2>
+                    <h4><i>Bachelors of Science, Computer Science</i></h4>
+                    <ul>{undergradList}</ul>
+                </Col>
+                <Col>
+                    <h2>University of Glasgow</h2>
+                    <h4><i>Masters of Science, Data Science</i></h4>
+                    <ul>{postgradList}</ul>
+                </Col>
+            </Row>
+            
+        </Container>
+    
+    )
 }
 
 export default Education;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,9 @@
 import react from "react";
-import { Component } from "react/cjs/react.development";
 
 
-class Projects extends Component {
-    render(){
-        return <h1>Projects Page</h1>
-    }
+const Projects = () => {
+    return <h1>Projects Page</h1>
+    
 }
 
 export default Projects;
